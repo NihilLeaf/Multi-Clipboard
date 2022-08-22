7821193f6dfe3bd4f8df98ddd07f56d4b2f45e1b
Thiago Cacio <94044199+NihilLeaf@users.noreply.github.com>
2022-08-21 22:28:29 -0300

    Função load, list e save adicionadas

--- multicliboard.py
+++ multicliboard.py
@@ -1,31 +1,62 @@
 import sys
+from time import sleep, time
 import clipboard
 import json
 
+SAVED_DATA = 'clipboard.json'
+
 #Fuction to add items to a json file
-def save_items(filepath, data):
+def save_data(filepath, data):
     with open(filepath, 'w') as f:
         json.dump(data, f)
 
 #Fuction to read the keys and values of the json file
-def load_json(filepath):
-    with open(filepath, 'r') as f:
-        data = json.load(f)
-        return data
-
+def load_data(filepath):
+    try:
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
 
 #Check if the value we pass after the py file on cmd or terminal is only 1
 if len(sys.argv) == 2:
     command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+
     #Check if the values are the choosen.
     if command == 'save':
-        print('save')
+        key = input('Enter a key: ')
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA ,data)
+        print('Data saved!')
+
     elif command == 'load':
-        print('load')
+        key = input('Enter a key: ')
+        if key in data:
+            clipboard.copy(data[key])
+            print('Data copied to clipboard!')
+        else:
+            print('Key not founded')
+            print("...")
+            sleep(1)
+            while True:
+                key = input('Enter a key again: ')
+                if key in data:
+                    clipboard.copy(data[key])
+                    print('Data copied to clipboard!')
+                    break
+                else:
+                    print('Key not founded')
+                    print('...')
+                    sleep(1)
+
     elif command == 'list':
-        print('list')
+        print(data)
+
     else:
         print('Unknown value')
+
 else:
     print('Type a value!')
 
