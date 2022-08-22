8ce3ceb9898565069f9e116f46ddcd9b10b9217e
Thiago Cacio <94044199+NihilLeaf@users.noreply.github.com>
2022-08-21 21:10:24 -0300

    Created the 2 fuctions of json file

--- multicliboard.py
+++ multicliboard.py
@@ -2,6 +2,18 @@ import sys
 import clipboard
 import json
 
+#Fuction to add items to a json file
+def save_items(filepath, data):
+    with open(filepath, 'w') as f:
+        json.dump(data, f)
+
+#Fuction to read the keys and values of the json file
+def load_json(filepath):
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+        return data
+
+
 #Check if the value we pass after the py file on cmd or terminal is only 1
 if len(sys.argv) == 2:
     command = sys.argv[1]
@@ -13,6 +25,7 @@ if len(sys.argv) == 2:
     elif command == 'list':
         print('list')
     else:
-        print('Unknow value')
-
+        print('Unknown value')
+else:
+    print('Type a value!')
 
